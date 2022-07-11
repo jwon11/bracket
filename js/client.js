91ddb0ba438247116cd7ff4c9779eb9c843799e5
sourbeatles3 <85785497+sourbeatles3@users.noreply.github.com>
2022-07-11 12:06:53 +0900

    Update client.js
    Update function width940

--- js/client.js
+++ js/client.js
@@ -26,7 +26,7 @@ let scrollEvent = function() {
     });
 
     width1217();
-    // width940();
+    width940();
 }
 
 let width1217 = function() {
@@ -65,26 +65,26 @@ let width1217 = function() {
 }
 
 let width940 = function() {
-    if(window.matchMedia("(max-width: 940px)").matches) {
-        let list = document.querySelectorAll(".client ul li");
-        let hiddenArray = [];
-        let openArray = [];
-        let array1 = [];
-        for(let i = 0; i < list.length; i++) {
-            array1.push(list[i]);
-        }
-        openArray = array1.slice(0, 4);
-        openArray = openArray.concat(array1.slice(4, 8));
-        openArray = openArray.concat(array1.slice(8, 12));
-        hiddenArray = array1.slice(12, 27);
-
-        for(let i = 0; i < hiddenArray.length; i++) {
-            hiddenArray[i].style.display = "none";
+    const itemList = document.querySelectorAll(".client ul li");
+    let array = [];
+    for(let i = 0; i < itemList.length; i++) {
+        array.push(itemList[i]);
+    }
+    
+    if(window.matchMedia("screen and (max-width: 940px)").matches) {
+        let visiArray = [];
+        visiArray = array.slice(0, 12);
+        let second = 0;
+        for(let i = 0; i < visiArray.length; i++) {
+            second = second + 0.1;
+            visiArray[i].style.animationDelay = second + "s";
         }
 
-        let more1 = document.getElementsByClassName("more-btn")[0];
-        more1.addEventListener("click", function(e) {
+        let more = document.getElementsByClassName("more-btn")[0];
+        more.addEventListener("click", function(e) {
             e.preventDefault();
+            let hiddenArray = [];
+            hiddenArray = array.slice(12, 27);
 
             let hSecond = 0;
             for(let i = 0; i < hiddenArray.length; i++) {
@@ -92,7 +92,7 @@ let width940 = function() {
                 hiddenArray[i].style.animationDelay = hSecond + "s";
                 hiddenArray[i].style.display = "block";
             }
-            more1.style.display = "none";
+            more.style.display = "none";
         });
     }
 }
